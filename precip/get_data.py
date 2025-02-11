6ee76ec51bed1f11c7189e7be58df07c1197aa4e
KingOliver666 <hlingqi@umich.edu>
2025-02-11 18:29:12 -0500

    new updates

--- precip/get_data.py
+++ precip/get_data.py
@@ -6,10 +6,19 @@ import pandas as pd
 
 base_url = "https://www.ncei.noaa.gov/data/daily-summaries/access"
 
-target_dir = "/home/kshedden/data/Teaching/precip"
+target_dir = "C:/Users/16343/Desktop/Stats-504-materials/precip"
 
 files = ["USW00094847.csv", "USW00012839.csv",]
 
 for f in files:
-    df = pd.read_csv(os.path.join(base_url, f))
-    df.to_csv(os.path.join(target_dir, f + ".gz"))
+    # Construct the full URL by concatenating base_url, "/", and the file name
+    url = f"{base_url}/{f}"
+
+    # Read the CSV directly from the URL
+    df = pd.read_csv(url)
+
+    # Create an output filename: "USW00094847.csv.gz", "USW00012839.csv.gz", etc.
+    output_path = os.path.join(target_dir, f + ".gz")
+    
+    # Write out to a gzip-compressed CSV file
+    df.to_csv(output_path, index=False, compression='gzip')
